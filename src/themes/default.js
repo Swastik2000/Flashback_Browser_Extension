2379962bc95e775ceb9a2d85127cfe4a263e4936
Soumava-221B <soumavadas06@gmail.com>
2022-03-28 23:04:07 +0530

    assets and themes

---
+++ src/themes/default.js
@@ -0,0 +1,12 @@
+import config from "./config";
+
+export default {
+  ...config,
+  palette: {
+    primary: "#488eff",
+    secondary: "#FFFFFF",
+    green: "#20bf6b",
+    yellow: "#f7b731",
+    red: "#ee5253",
+  },
+};
